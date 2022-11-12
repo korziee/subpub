22fc0bca02f06e7b734ed3a47c6ee5e4884b55e1
Kory Porter <kory.porter@hotmail.com>
2022-11-12 11:46:34 +1100

    stuiff

--- src/services/client/index.ts
+++ src/services/client/index.ts
@@ -1,5 +1,5 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
-import type { AppRouter } from "../server";
+import type { AppRouter } from "../router";
 
 const client = createTRPCProxyClient<AppRouter>({
   links: [
--- src/services/router/index.ts
+++ src/services/router/index.ts
@@ -1,4 +1,26 @@
+import { initTRPC } from "@trpc/server";
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import { z } from "zod";
 
-setInterval(() => {
-  console.log("Hello from a router");
-}, 100);
+export type AppRouter = typeof appRouter;
+
+const t = initTRPC.create();
+
+const appRouter = t.router({
+  zod: t.procedure.input(z.object({ name: z.string() })).mutation((req) => {
+    return req.input.name;
+  }),
+  greet: t.procedure
+    .input((val: unknown) => {
+      if (typeof val === "string") return val;
+      throw new Error(`Invalid input: ${typeof val}`);
+    })
+    .query(({ input }) => ({ greeting: `hello, ${input}!` })),
+});
+
+createHTTPServer({
+  router: appRouter,
+  createContext() {
+    return {};
+  },
+}).listen(2022);
